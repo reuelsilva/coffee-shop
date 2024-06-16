d7226cc48bef770266347e9c09f5e1276685e724
reuelsilva <reuelsilva19@gmail.com>
2024-06-16 20:19:48 -0300

    feat: adicionando a hero section

---
+++ src/app/components/Main/Hero/index.tsx
@@ -0,0 +1,15 @@
+import { ReactElement } from "react";
+import ButtonPrimary from "../ButtonPrimary";
+import "./styles.css"
+
+export default function Hero():ReactElement{
+    return(
+        <section className="hero-container">
+            <div className="hero-content">
+                <h1>Code com o melhor café da região</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est mollitia tempore incidunt voluptas sunt quam, blanditiis quo modi laudantium alias ab, itaque nam autem consequatur aspernatur a rem dolorum quis.</p>
+                <ButtonPrimary>Pegue o seu AGORA</ButtonPrimary>
+            </div>
+        </section>
+    )
+}
---
+++ src/app/components/Main/index.tsx
@@ -0,0 +1,10 @@
+import { ReactElement } from "react";
+import Hero from "./Hero";
+
+export default function Main():ReactElement{
+    return(
+        <main>
+            <Hero/>
+        </main>
+    )
+}
