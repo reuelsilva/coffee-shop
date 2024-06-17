07039ed4f011a6ccc218bad9ac01154449efb2bc
reuelsilva <reuelsilva19@gmail.com>
2024-06-17 10:19:23 -0300

    refactor: alterando classe hero-container para hero-section

--- src/app/components/Main/Hero/index.tsx
+++ src/app/components/Main/Hero/index.tsx
@@ -4,7 +4,7 @@ import "./styles.css"
 
 export default function Hero():ReactElement{
     return(
-        <section className="hero-container">
+        <section className="hero-section">
             <div className="hero-content">
                 <h1>Code com o melhor café da região</h1>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est mollitia tempore incidunt voluptas sunt quam, blanditiis quo modi laudantium alias ab, itaque nam autem consequatur aspernatur a rem dolorum quis.</p>
